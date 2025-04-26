9c2df4526974cf454c53642bdb76396e93a447c6
Minju Seo <48055421+going-doer@users.noreply.github.com>
2025-04-27 01:11:54 +0900

    Update 1.1_extract_config.py

--- codes/1.1_extract_config.py
+++ codes/1.1_extract_config.py
@@ -1,6 +1,9 @@
 import json
 import re
+import os
 import argparse
+import shutil
+from utils import extract_planning, content_to_json, format_json_data
 
 parser = argparse.ArgumentParser()
 
@@ -36,3 +39,28 @@ else:
             f.write(yaml_content)
     else:
         print("No YAML content found.")
+
+# ---------------------------------------
+
+artifact_output_dir=f"{output_dir}/planning_artifacts"
+
+os.makedirs(artifact_output_dir, exist_ok=True)
+
+context_lst = extract_planning(f'{output_dir}/planning_trajectories.json')
+
+arch_design = content_to_json(context_lst[1])
+logic_design = content_to_json(context_lst[2])
+
+formatted_arch_design = format_json_data(arch_design)
+formatted_logic_design = format_json_data(logic_design)
+
+with open(f"{artifact_output_dir}/1.1_overall_plan.txt", "w", encoding="utf-8") as f:
+    f.write(context_lst[0])
+
+with open(f"{artifact_output_dir}/1.2_arch_design.txt", "w", encoding="utf-8") as f:
+    f.write(formatted_arch_design)
+
+with open(f"{artifact_output_dir}/1.3_logic_design.txt", "w", encoding="utf-8") as f:
+    f.write(formatted_logic_design)
+
+shutil.copy(f"{output_dir}/planning_config.yaml", f"{artifact_output_dir}/1.4_config.yaml")
